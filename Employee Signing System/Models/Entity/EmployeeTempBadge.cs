e7ce5fe31405ea5300a8d2d8b49dfa99de5ad011
Sanidhya Dave <Sanidhya.Dave@hanu.com>
2023-04-01 17:10:55 +0530

    All repos from source folder at Hanu

--- Employee Signing System/Models/Entity/EmployeeTempBadge.cs
+++ Employee Signing System/Models/Entity/EmployeeTempBadge.cs
@@ -9,7 +9,6 @@ public partial class EmployeeTempBadge
 
     public int EmpId { get; set; }
 
-
     public string EmployeeFirstName { get; set; } = null!;
 
     public string EmployeeLastName { get; set; } = null!;
